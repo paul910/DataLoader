4ab6af8d81369402351d24b9267df4b55819ebac
paul910 <paul.ehrhard@web.de>
2023-06-09 23:49:30 +0200

    add progressbar

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
 from datasets.vulnerability_dataset import VulnerabilityDataset
 from datasets.parameter import PARAMS
+from tqdm import tqdm
 
 dataset = VulnerabilityDataset(**PARAMS)
 
-for i, data in enumerate(dataset):
+for i, data in enumerate(tqdm(dataset)):
     print(data)
