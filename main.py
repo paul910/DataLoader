26cbcfc9bf88227f88ab4055bf79b5ec12f3827d
paul910 <paul.ehrhard@web.de>
2023-06-10 00:02:12 +0200

    Update main.py

--- main.py
+++ main.py
@@ -2,9 +2,13 @@ from datasets.vulnerability_dataset import VulnerabilityDataset
 from datasets.parameter import PARAMS
 from tqdm import tqdm
 import torch
+import os
 
 dataset = VulnerabilityDataset(**PARAMS)
 
+if not os.path.exists("data/processed"):
+    os.makedirs("data/processed")
+
 for i, data in enumerate(tqdm(dataset)):
-    torch.save(data, f'{data.idx}_cpg.pt')
+    torch.save(data, f'data/processed/{data.idx}_cpg.pt')
     print(data)
