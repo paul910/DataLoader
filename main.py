5b18d9f3a7acf5036b9390c4e3398164f04fb444
paul910 <paul.ehrhard@web.de>
2023-06-09 23:59:24 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,8 +1,10 @@
 from datasets.vulnerability_dataset import VulnerabilityDataset
 from datasets.parameter import PARAMS
 from tqdm import tqdm
+import torch
 
 dataset = VulnerabilityDataset(**PARAMS)
 
 for i, data in enumerate(tqdm(dataset)):
+    torch.save(data, f'{data.idx}_cpg.pt')
     print(data)
